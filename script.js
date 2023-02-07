e17a6783f2b3aea94316c8ea2cf94d5727b5ba87
LastGovernor <p.nagy9@gmail.com>
2023-02-07 03:31:03 +0100

    BMI calculator added

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const linkedin = document.getElementById('linkedin');
 const github = document.getElementById('github');
+const bmiCalc = document.getElementById('portfolio-img');
 
 linkedin.addEventListener('click', function(){
-    window.open('https://www.linkedin.com/in/p%C3%A9ter-nagy-3ba647143', '_blank');
+    window.open('https://www.linkedin.com/in/p%C3%A9ter-nagy-3ba647143' , '_blank');
 });
 
 github.addEventListener('click', function(){
-    window.open('https://github.com/LastGovernor', '_blank');
+    window.open('https://github.com/LastGovernor' , '_blank');
+});
+
+bmiCalc.addEventListener('click', function(){
+    window.open('https://lastgovernor.github.io/BMI-Calculator/' , '_blank')
 });
