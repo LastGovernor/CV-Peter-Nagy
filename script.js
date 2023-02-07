5ef5bfe4fa16b565e5e138291cb30444499b892c
LastGovernor <p.nagy9@gmail.com>
2023-02-07 03:10:17 +0100

    Style added and all functioan as well

---
+++ script.js
@@ -0,0 +1,10 @@
+const linkedin = document.getElementById('linkedin');
+const github = document.getElementById('github');
+
+linkedin.addEventListener('click', function(){
+    window.open('https://www.linkedin.com/in/p%C3%A9ter-nagy-3ba647143', '_blank');
+});
+
+github.addEventListener('click', function(){
+    window.open('https://github.com/LastGovernor', '_blank');
+});
